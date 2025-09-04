720ed298a8d4572a2c7bfd65bd363a52afd87aff
Tyler <spearstyler5@gmail.com>
2025-09-04 13:15:17 -0400

    Homework

--- ch02_Tyler_Spears.js
+++ ch02_Tyler_Spears.js
@@ -62,9 +62,13 @@ let board = "";
 
 for (let y = 0; y < size; y++) {
   for (let x = 0; x < size; x++) {
-    board += (x + y) % 2 ? "#" : " ";
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
   }
   board += "\n";
 }
 
-console.log(board);
+console.log(board)
