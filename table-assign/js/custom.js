60c08a407ee422543ebb9bf737ff1a168123de15
Tyler <spearstyler5@gmail.com>
2025-10-07 13:23:50 -0400

    Table Homework

--- table-assign/js/custom.js
+++ table-assign/js/custom.js
@@ -5,14 +5,13 @@
  *******************************************************************/
 
  // The array of objects, one object for each artist.
- 
- const artists = [
-   {
-     name: "Ms Scandalous",
-     birthYear: 1985,
-     link: "https://www.youtube.com/watch?v=2FPivlfvxu0"
-   },
-   {
+const artists = [
+  {
+    name: "Ms Scandalous",
+    birthYear: 1985,
+    link: "https://www.youtube.com/watch?v=2FPivlfvxu0"
+  },
+  {
     name: "Juggy D",
     birthYear: 1981,
     link: "https://www.youtube.com/watch?v=1jAc_-FVjdI"
@@ -32,42 +31,31 @@
     birthYear: 1970,
     link: "https://www.youtube.com/watch?v=O95-w2gACuA"
   }
- ]
+];
 
-    const target = document.getElementById("bhangra");
-    const artistTable = makeTable(artists);
-    target.appendChild(artistTable);
-    
-    function makeTable(arr) {
-      const table = document.createElement("table");
-      const header = makeTableHeader(arr[0]);
-      table.appendChild(header);
-      for (let obj of arr) {
-        const row = makeTableRow(obj);
-        table.appendChild(row);
-      }
-      return table;
+const table = document.getElementById("bhangra");
+const thead = document.createElement("thead");
+const headerRow = document.createElement("tr");
+const columns = Object.keys(artists[0]);
+for (let col of columns) {
+  const th = document.createElement("th");
+  th.innerText = col;
+  headerRow.appendChild(th);
+}
+thead.appendChild(headerRow);
+table.appendChild(thead);
+const tbody = document.createElement("tbody");
+for (let artist of artists) {
+  const row = document.createElement("tr");
+  for (let website of columns) {
+    const cell = document.createElement("td");
+    if (website === "link") {
+      cell.innerHTML = `<a href="${artist[website]}" target="_blank">${artist[website]}</a>`;
+    } else {
+      cell.innerText = artist[website];
     }
-
-    function makeTableHeader(obj) {
-      const tr = document.createElement("tr");
-      for (let columnName of Object.keys(obj)) {
-        const th = document.createElement("th");
-        const data = columnName;
-        th.innerText = data;
-        tr.appendChild(th);
-      }
-      return tr;
-    }
-
-    function makeTableRow(obj) {
-      console.log(obj);
-      const row = document.createElement("tr");
-      for (let columnName of Object.keys(obj)) {
-        const data = document.createElement("td");
-        console.log(obj[columnName]);
-        data.innerText = obj[columnName];
-        row.appendChild(data);
-      }
-      return row;
-    }
+    row.appendChild(cell);
+  }
+   tbody.appendChild(row);
+}
+table.appendChild(tbody);
